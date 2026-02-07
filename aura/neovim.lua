5a9d930ceccd04419d50eef54dcbb71ad3859169
gitcoder89431 <leafx54@gmail.com>
2026-02-07 00:50:17 -0500

    Fix Aura Neovim colorscheme loading

--- aura/neovim.lua
+++ aura/neovim.lua
@@ -1,17 +1,38 @@
-return {
-	{
-		"baliestri/aura-theme",
+local aura_repo = "/home/dev/repos/dev/aura-theme"
+local aura_local = aura_repo .. "/packages/neovim"
+local aura_plugin = {
+	"baliestri/aura-theme",
+	lazy = false,
+	priority = 1000,
+	rtp = "packages/neovim",
+}
+
+if vim.fn.isdirectory(aura_repo) == 1 then
+	aura_plugin = {
+		dir = aura_repo,
+		name = "aura-theme",
 		lazy = false,
 		priority = 1000,
-		config = function(plugin)
-			vim.opt.rtp:append(plugin.dir .. "/packages/neovim")
-			vim.cmd("colorscheme aura-soft-dark")
-		end,
-	},
+		rtp = "packages/neovim",
+	}
+end
+
+return {
+	aura_plugin,
 	{
 		"LazyVim/LazyVim",
 		opts = {
-			colorscheme = "aura-soft-dark",
+			colorscheme = function()
+				if vim.fn.isdirectory(aura_local) == 1 then
+					vim.opt.rtp:prepend(aura_local)
+				else
+					local aura_lazy = vim.fn.stdpath("data") .. "/lazy/aura-theme/packages/neovim"
+					if vim.fn.isdirectory(aura_lazy) == 1 then
+						vim.opt.rtp:prepend(aura_lazy)
+					end
+				end
+				vim.cmd.colorscheme("aura-soft-dark")
+			end,
 		},
 	},
 }
